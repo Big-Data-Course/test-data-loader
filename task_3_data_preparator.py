2610d2e3d8580870e231cf3b48433c1fcd026244
gchevykalov <71878379+gchevykalov@users.noreply.github.com>
2023-12-03 19:53:27 +0300

    Add files via upload

--- task_3_data_preparator.py
+++ task_3_data_preparator.py
@@ -1,10 +1,14 @@
 from astropy.io import ascii
+from sys import argv
 import os
 
 data_dir = "loaded_data"
 
-def prepare_data(dir):
+def prepare_data(dir, indFrom, indTo):
     for filename in os.listdir(data_dir):
+        ind = int(filename.split("_")[-1])
+        if ind < indFrom or ind >= indTo:
+            continue
         print(filename)
         fin = os.path.join(data_dir, filename)
         fout = os.path.join(dir, filename)
@@ -19,4 +23,7 @@ def prepare_data(dir):
 if __name__ == "__main__":
     if not os.path.exists("task_3_data"):
         os.makedirs("task_3_data")
-    prepare_data("task_3_data")
+    if len(argv) != 3:
+        prepare_data("task_3_data")
+    else:
+        prepare_data("task_3_data", int(argv[1]), int(argv[2]))
